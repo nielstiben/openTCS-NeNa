5ce0dc905e84d31fc0192edc6ef53ab67ea39c69
Niels Tiben <nielstiben@outlook.com>
2020-04-29 17:26:06 +0200

    Write additional test and several minor bug fixes

---
+++ openTCS-NeNa-CommAdapter-ROS2/src/test/java/nl/saxion/nena/opentcs/commadapter/ros2/kernel/adapter/transport_order/ExecuteTransportOrderWorkflowTest.java
@@ -0,0 +1,7 @@
+package nl.saxion.nena.opentcs.commadapter.ros2.kernel.adapter.transport_order;
+
+/**
+ * @author Niels Tiben <nielstiben@outlook.com>
+ */
+public class ExecuteTransportOrderWorkflowTest {
+}
--- openTCS-NeNa-CommAdapter-ROS2/src/test/java/nl/saxion/nena/opentcs/commadapter/ros2/unit/transport_order/ExecuteOperationWorkflowTest.java
+++
@@ -1,9 +0,0 @@
-package nl.saxion.nena.opentcs.commadapter.ros2.unit.transport_order;
-
-/**
- *
- * @author Niels Tiben
- */
-public class ExecuteOperationWorkflowTest {
-
-}
